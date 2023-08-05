767fc71054a88c4480bfc336fb064ac145d809b5
evan <evan.lindsay223@gmail.com>
2023-08-04 21:28:29 -0700

    adding some comments

--- src/main/kotlin/com/wishlist/wishlistapp/WishlistappApplication.kt
+++ src/main/kotlin/com/wishlist/wishlistapp/WishlistappApplication.kt
@@ -5,7 +5,8 @@ import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class WishlistappApplication
-
+//Completed?
+//Features to add ... 
 fun main(args: Array<String>) {
 	runApplication<WishlistappApplication>(*args)
 }
